d3d82f0c5c6c55bd67c76de3a853c35435685d02
Josh Petersen <jpetersen841@gmail.com>
2015-08-07 09:30:52 -0400

    in progress.

--- Workshop3/index.js
+++ Workshop3/index.js
@@ -1,5 +1,86 @@
 /// <reference path="./typings/tsd.d.ts" />
 var myApp = angular.module('Workshop3', []);
-myApp.controller("MusicController", function ($scope) {
+myApp.factory('SongSelectionDTO', function () {
+    function SongSelectionDTO(title, severity) {
+        this.title = title;
+        this.severity = severity;
+    }
+    return SongSelectionDTO;
+});
+myApp.factory('UserDTO', function (SongSelectionDTO) {
+    function UserDTO(name, age, songSelections) {
+        this.name = name;
+        this.age = age;
+        this.songSelections = songSelections;
+    }
+    return UserDTO;
+});
+myApp.value('GenreList', [
+    {
+        genreName: 'Pop',
+        songList: [
+            {
+                name: 'Generic',
+                severity: 'clean'
+            },
+            {
+                name: 'Song',
+                severity: 'clean'
+            },
+            {
+                name: 'Here',
+                severity: 'clean'
+            },
+            {
+                name: 'BlahBlahBlah',
+                severity: 'clean'
+            },
+        ]
+    },
+    {
+        genreName: 'Rock',
+        songList: [
+            {
+                name: 'Repeat',
+                severity: 'explicit'
+            },
+            {
+                name: 'Stuff',
+                severity: 'explicit'
+            },
+            {
+                name: 'Here',
+                severity: 'explicit'
+            },
+            {
+                name: 'Songs',
+                severity: 'clean'
+            },
+        ]
+    },
+    {
+        genreName: 'Rap',
+        songList: [
+            {
+                name: 'Do',
+                severity: 'explicit'
+            },
+            {
+                name: 'Not',
+                severity: 'explicit'
+            },
+            {
+                name: 'Want',
+                severity: 'explicit'
+            },
+        ]
+    }
+]);
+myApp.controller("MusicController", function ($scope, UserDTO, SongSelectionDTO, GenreList) {
+    var self = this;
+    self.genreList = angular.copy(GenreList);
+    self.addRemoveSong = function (song) {
+    };
+    console.log(self.genreList);
 });
 //# sourceMappingURL=index.js.map
--- Workshop3/index.ts
+++ Workshop3/index.ts
@@ -2,6 +2,104 @@
 
 var myApp = angular.module('Workshop3', []);
 
-myApp.controller("MusicController", function($scope){
+interface ISongSelectionDTO{
+    title:string;
+    severity:string;
+}
 
+myApp.factory('SongSelectionDTO', function(){
+    function SongSelectionDTO(title:string, severity:string){
+        this.title = title;
+        this.severity = severity;
+    }
+
+    return SongSelectionDTO;
+});
+
+myApp.factory('UserDTO', function(SongSelectionDTO){
+    function UserDTO(name:string, age:number, songSelections:ISongSelectionDTO[]){
+        this.name = name;
+        this.age = age;
+        this.songSelections = songSelections;
+    }
+
+    return UserDTO;
+});
+
+myApp.value('GenreList',
+    [
+        {
+            genreName: 'Pop',
+            songList: [
+                {
+                    name: 'Generic',
+                    severity: 'clean'
+                },
+                {
+                    name: 'Song',
+                    severity: 'clean'
+                },
+                {
+                    name: 'Here',
+                    severity: 'clean'
+                },
+                {
+                    name: 'BlahBlahBlah',
+                    severity: 'clean'
+                },
+            ]
+        },
+        {
+            genreName: 'Rock',
+            songList:[
+                {
+                    name: 'Repeat',
+                    severity: 'explicit'
+                },
+                {
+                    name: 'Stuff',
+                    severity: 'explicit'
+                },
+                {
+                    name: 'Here',
+                    severity: 'explicit'
+                },
+                {
+                    name: 'Songs',
+                    severity: 'clean'
+                },
+
+            ]
+        },
+        {
+            genreName: 'Rap',
+            songList: [
+                {
+                    name: 'Do',
+                    severity: 'explicit'
+                },
+                {
+                    name: 'Not',
+                    severity: 'explicit'
+                },
+                {
+                    name: 'Want',
+                    severity: 'explicit'
+                },
+
+            ]
+        }
+    ]
+);
+
+myApp.controller("MusicController", function($scope, UserDTO, SongSelectionDTO, GenreList){
+    var self = this;
+
+    self.genreList = angular.copy(GenreList);
+
+    self.addRemoveSong = function(song):void{
+
+    };
+
+    console.log(self.genreList);
 });
