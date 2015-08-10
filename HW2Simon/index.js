21daa5d005560460e790ed2473b33329de367dfa
Josh Petersen <jpetersen841@gmail.com>
2015-08-10 10:11:47 -0400

    homework 2 init.

---
+++ HW2Simon/index.js
@@ -0,0 +1,3 @@
+/// <reference path="./refs.d.ts" />
+angular.module('simonApp', []);
+//# sourceMappingURL=index.js.map
---
+++ HW2Simon/index.ts
@@ -0,0 +1,2 @@
+/// <reference path="./refs.d.ts" />
+angular.module('simonApp', []);
